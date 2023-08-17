759a2750db0311951dc7f8c523f7e6ec1c07fbc8
Thackerie <thackerie164@gmail.com>
2023-08-17 07:18:54 -0400

    added navbar and correlating hrefs added login/logou/register functionality

--- WalletWise/urls.py
+++ WalletWise/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("signup", views.signup, name="signup"),
+    path("profile/<str:username>", views.profile, name="profile"),
+    path("dashboard/<str:username>", views.dashboard, name="dashboard")
 ]
