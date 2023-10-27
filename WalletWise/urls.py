5429fd4f9f67df4a4d51403afad97693aa6c10ed
Thackerie <thackerie164@gmail.com>
2023-10-27 14:25:51 +0200

    replaced index page with dashboard page

--- WalletWise/urls.py
+++ WalletWise/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.dashboard_view, name="dashboard"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("signup", views.signup, name="signup"),
